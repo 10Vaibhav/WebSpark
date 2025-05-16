991041d6d4bdf93d08a53ab513c6aeba58c8c9fc
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-05-16 18:07:30 +0530

    Create a Template Endpoint that identify project type based on user prompt (i.e, React or Node)

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,29 +1,56 @@
 import * as dotenv from "dotenv";
 import Anthropic from "@anthropic-ai/sdk";
-import { getSystemPrompt } from "./prompts";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import express from "express";
+import { TextBlock } from "@anthropic-ai/sdk/resources/messages";
+import { basePrompt as nodeBasePrompt } from "./defaults/node"
+import { basePrompt as reactBasePrompt } from "./defaults/react";
+import fs from "fs";
 dotenv.config();
+
 
 // It Automatically Set API Key From Environment
 const anthropic = new Anthropic();
+const app = express();
+app.use(express.json());
 
-async function main() {
-    anthropic.messages.stream({
-    messages: [{
-        role: 'user',
-        content: "For all designs I ask you to make, have them be beautiful, not cookie cutter, Make webpages that are fully featured and worthy for production.\n\nBy default, this template supports JSX syntax with Tailwind CSS classes, React hooks, and Lucide React for icons. Do not install other packages for UI \n\nUse stock photos from unsplash where appropriate, only valid URLs you know exist. Do not download the images, only link to them in image tags.\n\n"
-    },{
-        role: 'user',
-        content: "Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n{{BASE_PROMPT}} \n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n - .gitignore\n - package-lock.json\n - .bolt/prompt"
-    },{
-        role: 'user',
-        content: "Create a simple todo app"
-    }],
-    model: 'claude-3-7-sonnet-20250219',
-    max_tokens: 8000,
-    system: getSystemPrompt()
-}).on('text', (text) => {
-    console.log(text);
-});
-}
+app.post("/template", async (req, res) => {
+    const prompt = req.body.prompt;
 
-main();
+    const response = await anthropic.messages.create({
+        messages: [{
+            role: 'user',
+            content: prompt
+        }],
+        model: 'claude-3-7-sonnet-20250219',
+        max_tokens: 200,
+        system: "Return either node or react based on what do you think this project should be. Only return a Single word either 'node' or 'react'. Do not return anything extra"
+    });
+
+    const answer = (response.content[0] as TextBlock).text; // react or node
+
+    if (answer == "react") {
+        res.json({
+            prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n - .gitignore\n - package-lock.json\n - .bolt/prompt`],
+            uiPrompts: [reactBasePrompt]
+        });
+        return;
+    }
+
+    if (answer == "node") {
+        res.json({
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n - .gitignore\n - package-lock.json\n - .bolt/prompt`],
+            uiPrompts: [nodeBasePrompt]
+        });
+        return;
+    }
+
+    res.status(403).json({ message: "You cant access this" });
+    return;
+
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is running on 3000")
+})
+
